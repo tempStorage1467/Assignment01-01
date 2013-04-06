169a6cec25c79b5ab445d5923e016c16711883a6
Eric Beach <ebeach@google.com>
2013-04-06 11:51:30 -0400

    Default Project

---
+++ ConsecutiveHeads.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: ConsecutiveHeads.cpp
+ * --------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Consecutive Heads problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+int main() {
+    // [TODO: fill in the code]
+    return 0;
+}
