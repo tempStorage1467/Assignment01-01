faf754813da8579dabbe11c45f6d03e3fa7ac99d
Eric Beach <ebeach@google.com>
2013-04-06 14:03:50 -0400

    Basic working version with Classes

--- ConsecutiveHeads.cpp
+++ ConsecutiveHeads.cpp
@@ -1,17 +1,20 @@
 /*
  * File: ConsecutiveHeads.cpp
  * --------------------------
- * Name: [TODO: enter name here]
+ * Name: Eric Beach
  * Section: [TODO: enter section leader here]
  * This file is the starter project for the Consecutive Heads problem.
- * [TODO: rewrite the documentation]
  */
 
 #include <iostream>
 #include "console.h"
+#include "CoinFlipSimulator.h"
+
 using namespace std;
 
 int main() {
-    // [TODO: fill in the code]
+    CoinFlipSimulator simulator = CoinFlipSimulator();
+    simulator.determineNumFlipsRequired();
+    simulator.printSimulation();
     return 0;
 }
